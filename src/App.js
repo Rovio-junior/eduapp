c0045785ac07a6f18a451bd08a2ab98992affa59
Mohamed Maaninou <mohamed.maaninou@gmail.com>
2024-09-11 12:45:26 +0100

    Updated project files and added new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Layout } from "./components/common/Layout"
+import { Home } from "./pages/Home"
+import { BlogSinglePage } from "./components/common/BlogSinglePage"
+import { About } from "./pages/About"
+import { Courses } from "./pages/Courses"
+import { Blog } from "./pages/Blog"
+import { Instructor } from "./pages/Instructor"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path='/about'
+            element={
+              <Layout>
+                <About />
+              </Layout>
+            }
+          />
+          <Route
+            path='/courses'
+            element={
+              <Layout>
+                <Courses />
+              </Layout>
+            }
+          />
+          <Route
+            path='/instructor'
+            element={
+              <Layout>
+                <Instructor />
+              </Layout>
+            }
+          />
+          <Route
+            path='/blog'
+            element={
+              <Layout>
+                <Blog />
+              </Layout>
+            }
+          />
+          <Route
+            path='/single-blog'
+            element={
+              <Layout>
+                <BlogSinglePage />
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
